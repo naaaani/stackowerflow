31489a728c86a7a2bb1e9697c2e748904cd3dede
szbubu <106345041+szbubu@users.noreply.github.com>
2023-10-25 14:05:47 +0200

    implement frontend for the project, unfinished

--- frontend/sof/src/Answer.jsx
+++ frontend/sof/src/Answer.jsx
@@ -3,12 +3,12 @@
 
 
 function Answer(props){
-
   return (
     <>
     <div>Answer: {props.answer.body}</div>
     <h2>created at: {props.answer.created_at}</h2>
     <h2>likes: {props.answer.like_count}</h2>
+    <button onClick={()=>{props.onLikeClick(props.answer.id)}}>Like this answer</button>
     </>
   )
 
