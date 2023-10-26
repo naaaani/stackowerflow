de7c1a3fa4e6b577a8277901369f69d3d34f9a97
szbubu <106345041+szbubu@users.noreply.github.com>
2023-10-26 10:19:39 +0200

    remove cors because proxy is working

--- src/main/java/com/codecool/stackoverflowtw/StackoverflowTwApplication.java
+++ src/main/java/com/codecool/stackoverflowtw/StackoverflowTwApplication.java
@@ -1,12 +1,10 @@
 package com.codecool.stackoverflowtw;
 
-
 import com.codecool.stackoverflowtw.dao.QuestionsDAO;
 import com.codecool.stackoverflowtw.dao.QuestionsDaoJdbc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 
 
 @SpringBootApplication
